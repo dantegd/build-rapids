b5fa8415b7dc0fffb50f5246b3679b9d1e21f2b0
mt-jones <m.t.j.rider@icloud.com>
2018-12-12 12:58:46 -0700

    fix typo in cuml build

--- utils/build-cuml.sh
+++ utils/build-cuml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd cuml && mkdir -p build && cd build && \
+cd cuml/cuML && mkdir -p build && cd build && \
 cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
 make -j && make install && \
 cd ../../python && python setup.py install && \
