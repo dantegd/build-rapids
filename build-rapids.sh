fcd57296c60592546899b26d4fe6afcd211d055e
mt-jones <m.t.j.rider@icloud.com>
2018-12-12 11:43:55 -0700

    updating all build scripts

--- build-rapids.sh
+++ build-rapids.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+export RAPIDS_ROOT=$(pwd)
+
 source activate rapids && \
 bash utils/build-cudf.sh && \
-bash utils/build-cuml.sh && \
-bash utils/build-cugraph.sh && \
+#bash utils/build-cuml.sh && \
+#bash utils/build-cugraph.sh && \
 bash utils/build-xgboost.sh && \
 bash utils/build-dask-cudf.sh && \
 bash utils/build-dask-xgboost.sh && \
-bash utils/build-dask-xgboost && \
 echo -e "\n successfully built and installed RAPIDS \n"
