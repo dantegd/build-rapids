6c049cec334e52b69960ec920f1f78c1f6923475
mt-jones <m.t.j.rider@icloud.com>
2018-12-12 13:00:48 -0700

    fix install step for cuml python

--- utils/build-cuml.sh
+++ utils/build-cuml.sh
@@ -3,5 +3,6 @@
 cd cuml/cuML && mkdir -p build && cd build && \
 cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
 make -j && make install && \
-cd ../../python && python setup.py install && \
+cd ../../python && python setup.py build_ext --inplace && \
+python setup.py install && \
 cd $RAPIDS_ROOT
