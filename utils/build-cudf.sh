5a755eec8aa3200f283f0790533acf42c99dc06d
mt-jones <m.t.j.rider@icloud.com>
2018-12-26 16:06:31 -0700

    removing build tests for cudf

--- utils/build-cudf.sh
+++ utils/build-cudf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd cudf/cpp && mkdir -p build && cd build && \
-cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
+cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
+		 -DBUILD_TESTS=OFF && \
 make -j && make install && \
 make python_cffi && make install_python && \
 cd ../../python && python setup.py build_ext --inplace && \
