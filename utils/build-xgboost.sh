a041b9233153fa2e2c9fc7419ca4b1b2e1ea5aa2
Dante Gama Dessavre <danteg@nvidia.com>
2019-01-06 18:33:05 -0600

    FIX Removed unnecessary components

--- utils/build-xgboost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd xgboost && mkdir -p build && cd build && \
-cmake .. -DUSE_NCCL=ON -DUSE_CUDA=ON \
-         -DGDF_INCLUDE_DIR=$CONDA_PREFIX/include \
-         -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
-make -j && make install && \
-cd ../python-package && python setup.py install && \
-cd $RAPIDS_ROOT
