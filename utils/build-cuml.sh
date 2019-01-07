c8e093cdea5f05e428986d409bd6ce2dab1ce649
Dante Gama Dessavre <danteg@nvidia.com>
2019-01-06 18:31:48 -0600

    ENH Added option to specify ARCH_GPU flag for compilation

--- utils/build-cuml.sh
+++ utils/build-cuml.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cd cuml/cuML && mkdir -p build && cd build && \
-cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
-make -j && make install && \
-cd ../../python && python setup.py build_ext --inplace && \
-python setup.py install && \
-cd $RAPIDS_ROOT
+if [ -n "$GPU_ARCHS" ]; then
+    cd cuml/cuML && mkdir -p build && cd build && \
+    cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DGPU_ARCHS="$GPU_ARCHS"&& \
+    make -j && make install && \
+    cd ../../python && python setup.py build_ext --inplace && \
+    python setup.py install && \
+    cd $RAPIDS_ROOT
+else
+    cd cuml/cuML && mkdir -p build && cd build && \
+    cmake .. -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX && \
+    make -j && make install && \
+    cd ../../python && python setup.py build_ext --inplace && \
+    python setup.py install && \
+    cd $RAPIDS_ROOT
+fi
